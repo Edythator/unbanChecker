17394238833d1df4a713a630439dae316e00f596
Edythator <edythator@gmail.com>
2019-11-11 23:31:26 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace unbanChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+            string name = string.Empty;
+
+            if (args.Length > 0)
+                name = args[0];
+
+            else
+            {
+                Console.WriteLine("id/username");
+                name = Console.ReadLine();
+            }
+
+            while (true)
+            {
+                try
+                {
+                    string s = wc.DownloadString("https://old.ppy.sh/u/" + name);
+                    if (s != null)
+                    {
+                        Console.WriteLine("Unbanned!! Congratulations, " + name + "!");
+                        break;
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("\nsnoozing :zzz:");
+                    Thread.Sleep(50000);
+                }
+            }
+
+            Console.ReadLine();
+            wc.Dispose();
+        }
+    }
+}
